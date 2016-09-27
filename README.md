97bcc784f6b534b8aa2e5699255922102bafc5a6
Luis Gustavo Fernandez Batres <fb012@grupocassa.com>
2016-09-27 12:48:10 -0600

    Versión inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+# simple-master-detail
+Mantenimiento maestro-detalle simple con angular2 e indexedDB
---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component }  from '@angular/core';
+
+@Component({
+    selector: 'simple-master-detail',
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css'] 
+})
+class AppComponent { }
+
+export { AppComponent };
